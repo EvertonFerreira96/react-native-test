287a84737b56c5308b09e0f92e29ae8a040e5dbe
Everton Ferreira <ferreira.everton@icloud.com>
2021-09-24 18:19:16 -0300

    release: Components & Screens in Feeds

--- mobile/src/main/App.tsx
+++ mobile/src/main/App.tsx
@@ -2,9 +2,9 @@ import { ApplicationContextProviderManagement } from '../presentation/hooks/fact
 import React from 'react';
 import { AuthenticationSignInScreen } from '../presentation/jsx/screens/authentication/sing-in';
 import { Routes } from './routes/index.routes';
-import { YellowBox } from 'react-native';
 
 export const App: React.FC = () => {
+  console.disableYellowBox = true
   return (
 
     <ApplicationContextProviderManagement>
